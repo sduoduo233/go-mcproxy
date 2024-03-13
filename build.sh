e894e2b39fc06ad882dc3e3731addbe388c14977
duoduo song <sduoduo233@gmail.com>
2024-03-13 10:34:31 +0800

    update build script

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ export GOARCH=amd64
 export CGO_ENABLED=0
 
 export GOOS=windows
-go build -o ./build/mcproxy.exe -trimpath -ldflags "-w"
+go build -o ./build/mcproxy.exe -trimpath -ldflags "-s -w"
 
 export GOOS=linux
-go build -o ./build/mcproxy -trimpath -ldflags "-w"
+go build -o ./build/mcproxy -trimpath -ldflags "-s -w"
